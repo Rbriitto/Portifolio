221002750d03732018fa842add9f42dc8483e7b8
Rodrigo de Britto Aquino <rodrigobritto.web@gmail.com>
2025-10-20 16:05:11 -0300

    criando

--- Loja de Roupas/code/dataset.py
+++ Loja de Roupas/code/dataset.py
@@ -7,13 +7,14 @@ clientes = pd.read_csv("../data/clientes.csv")
 marketing = pd.read_csv("../data/marketing.csv")
 estoque = pd.read_csv("../data/estoque.csv")
 
+
+
+
+
 clientes.rename(columns={"CPF": "CPF_Cliente"}, inplace=True)
 
 vendas_por_clientes = vendas.merge(clientes,on="CPF_Cliente")
-
-
-
 df = vendas.merge(produtos, on="Nome_Produto", how="left")
 
-df
-
+pd.to_datetime(df['Data_Hora'], errors='coerce')
+print(df.dtypes)
--- Loja de Roupas/code/utils.py
+++ Loja de Roupas/code/utils.py
@@ -4,4 +4,14 @@ import streamlit as st
 import time
 
 
+#onde as vendas foram realizadas
+def format_number(value, prefix = ''):
+    for unit in ['', 'mil']:
+        if value < 1000:
+            return f'{prefix} {value:.2f} {unit}'
+        value /= 1000
+    return f'{prefix}{value:.2f} milhões'
+
+
+#produtos mais vendidos por período
 
